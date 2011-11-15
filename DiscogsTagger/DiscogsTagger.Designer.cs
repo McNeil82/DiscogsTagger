4fd8f0ac5b8f983a978507a3d120ca144ed1a0a8
McNeil <mcneil@gmx.net>
2011-11-15 15:58:02 +0100

    name refactoring

---
+++ DiscogsTagger/DiscogsTagger.Designer.cs
@@ -0,0 +1,34 @@
+﻿namespace DiscogsTagger
+{
+    partial class DiscogsTagger
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 265);
+            this.Name = "MainFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Discogs-Tagger";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
--- DiscogsTagger/DiscogsTagger.cs
+++ DiscogsTagger/DiscogsTagger.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace DiscogsTagger
 {
-    public partial class Form1 : Form
+    public partial class DiscogsTagger : Form
     {
-        public Form1()
+        public DiscogsTagger()
         {
             InitializeComponent();
         }
