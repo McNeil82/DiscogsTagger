4593af0fb8cef00125f0ad0af776b712e51d6cf6
McNeil <mcneil@gmx.net>
2011-11-15 18:26:54 +0100

    restructured and first test

---
+++ DiscogsTagger/DiscogsTagger.Components.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace DiscogsTagger
+{
+    partial class DiscogsTagger
+    {
+        private IContainer components = null;
+        private TextBox resleaseNumberTextBox;
+        private Button searchButton;
+        private TextBox debugTextBox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            resleaseNumberTextBox = new TextBox();
+            searchButton = new Button();
+            debugTextBox = new TextBox();
+            SuspendLayout();
+
+            resleaseNumberTextBox.Location = new Point(13, 13);
+            resleaseNumberTextBox.Name = "txb_release";
+            resleaseNumberTextBox.Size = new Size(100, 20);
+            resleaseNumberTextBox.TabIndex = 0;
+            resleaseNumberTextBox.KeyPress += new KeyPressEventHandler(releaseNumberTextBoxKeyPressEvent);
+
+            searchButton.Location = new Point(197, 9);
+            searchButton.Name = "btn_search";
+            searchButton.Size = new Size(75, 23);
+            searchButton.TabIndex = 1;
+            searchButton.Text = "Search";
+            searchButton.UseVisualStyleBackColor = true;
+            searchButton.Click += new EventHandler(searchButtonClickEvent);
+            
+            debugTextBox.Location = new Point(13, 187);
+            debugTextBox.Multiline = true;
+            debugTextBox.Name = "txb_debug";
+            debugTextBox.Size = new Size(259, 66);
+            debugTextBox.TabIndex = 2;
+
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new Size(284, 265);
+            Controls.Add(debugTextBox);
+            Controls.Add(searchButton);
+            Controls.Add(resleaseNumberTextBox);
+            Name = "DiscogsTagger";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Discogs-Tagger";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
+
--- DiscogsTagger/DiscogsTagger.cs
+++ DiscogsTagger/DiscogsTagger.cs
@@ -6,14 +6,37 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace DiscogsTagger
 {
     public partial class DiscogsTagger : Form
     {
+        private string releaseNumber = "";
+
         public DiscogsTagger()
         {
             InitializeComponent();
+        }
+
+        private void handleReleaseNumber()
+        {
+            if (validReleaseNumber())
+            {
+                releaseNumber = resleaseNumberTextBox.Text;
+            }
+            else
+            {
+                releaseNumber = "";
+            }
+
+            debugTextBox.Text = releaseNumber;
+        }
+
+        private bool validReleaseNumber()
+        {
+            int intParse;
+            return Int32.TryParse(resleaseNumberTextBox.Text, NumberStyles.None, null, out intParse);
         }
     }
 }
